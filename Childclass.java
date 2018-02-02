41dbc84762ce2821af8b8df2ca145e174d857979
ipsita <pattnaik.ipsita13@gmail.com>
2018-02-02 21:27:34 +0530

    test java file

---
+++ Childclass.java
@@ -0,0 +1,15 @@
+package Testingjava;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Childclass 
+{
+	public static void main (String args[]) throws FileNotFoundException {
+
+	FileInputStream f=new FileInputStream("./jk.txt");
+}
+}
+
+
+
---
+++ Parentclass.java
@@ -0,0 +1,5 @@
+package Testingjava;
+
+public class Parentclass {
+
+}
